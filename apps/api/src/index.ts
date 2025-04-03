908c235f35e93ebb9fb1907c7559236f922c1003
electroluxcode <electroluxcoder@gmail.com>
2025-04-03 10:53:49 +0800

    feat: ts引入支持

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
@@ -9,7 +9,7 @@ import routes from './routes';
 // Load environment variables
 dotenv.config();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 // Middlewares
